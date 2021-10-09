a5fe03e8a8617401fa5c7a5403ec6f25fa2dfce3
Nikos Chasiotis <hasiotis@gmail.com>
2021-10-09 20:56:48 +0300

    Trying out live version

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -eux
 
+# Install some common packages
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y \
+    rsync less mc acl curl links lynx vim lsof ssh host \
+    htop ntp mutt git-core iotop make zip unzip \
+    sysstat dstat httpie jq python3-pip
+apt-get autoremove --purge -y
+
 # Add sysadmin user to sudoers.
 echo "sysadmin        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
